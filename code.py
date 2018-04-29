46526c6d34361dd6c00206165a4f450011062d11
mazonline <mazonline@users.noreply.github.com>
2018-04-29 14:24:15 -0700

    Update code.py

--- code.py
+++ code.py
@@ -10,3 +10,6 @@ import unittest, time, re
         self.selenium = selenium("localhost", 4444, "*firefox","http://mazonline.github.io/index.html/")
         self.selenium.start()
         # We instantiate and start the browser
+
+      
+      #https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux32.tar.gz
