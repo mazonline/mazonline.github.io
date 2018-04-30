87165f17a5bec1914f1ed40115dd9e44255ee267
mazonline <mazonline@users.noreply.github.com>
2018-04-30 09:40:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash 
-
+mkdir /work/
+cd /work 
 apt-get update 
 apt-get install -y wget
 apt-get install -y python-pip
 apt-get install -y vim
 apt-get install -y curl
+apt-get install -y firefox
 pip install selenium 
+
+#links 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux32.tar.gz
+wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
